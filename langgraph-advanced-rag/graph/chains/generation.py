f8d6b79211d7f06bce980fa3d2ef86507a8a7ddf
Anthony Kugel <anthonykugel@gmail.com>
2025-04-20 07:57:56 -0500

    added pirate functionality

--- langgraph-advanced-rag/graph/chains/generation.py
+++ langgraph-advanced-rag/graph/chains/generation.py
@@ -5,4 +5,16 @@ from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(temperature=0)
 prompt = hub.pull("rlm/rag-prompt")
 
-generation_chain = prompt | llm | StrOutputParser()
+#generation_chain = prompt | llm | StrOutputParser()
+
+
+
+# enter pirate mode
+from langchain_core.prompts import PromptTemplate
+
+pirate_prompt_template = PromptTemplate.from_template(
+    template="take the {text} and answer like a pirate"
+)
+generation_chain  = (
+    prompt | llm | StrOutputParser() | pirate_prompt_template | llm | StrOutputParser()
+)
